ed3dafd6b8612eaa74fcd89f6f6581aab6048c21
Mason Barnes <barnesmason980@gmail.com>
2024-06-03 10:30:08 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Mason"
+const welcomeMessage = "Welcome to Flatbook, Mason!"
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, MASON!'
+const shortGreeting = "Welcome, M!"
